8622700177349b42ed4736bae91cdb36b3cf70d0
Rus Kuzmin <ruskuzmin@icloud.com>
2020-04-11 02:11:17 +0100

    Code and Scene cleanup

--- Assets/TerrainPool.cs
+++ Assets/TerrainPool.cs
@@ -7,30 +7,32 @@ public class TerrainPool : MonoBehaviour
     public GameObject[] treePrefabs;
     public GameObject[] rockPrefabs;
     //public GameObject[] Misc;
-
     private static int treeCount = 1000;
     private static int rockCount = 150;
     private static GameObject[] trees;
     private static GameObject[] rocks;
-    // Start is called before the first frame update
     void Start()
     {
         trees = new GameObject[treeCount];
         for(int i = 0; i < treeCount; i++) {
             int treeIndex = Random.Range(0, treePrefabs.Length);
-            trees[i] = Instantiate(treePrefabs[treeIndex], Vector3.zero, Quaternion.identity) as GameObject;
+            trees[i] = Instantiate(treePrefabs[treeIndex], 
+                Vector3.zero, 
+                Quaternion.identity) as GameObject;
+            trees[i].transform.SetParent(this.transform);
             trees[i].SetActive(false);
         }
 
         rocks = new GameObject[rockCount];
         for(int i = 0; i < rockCount; i++) {
             int rockIndex = Random.Range(0, rockPrefabs.Length);
-            rocks[i] = Instantiate(rockPrefabs[rockIndex], Vector3.zero, Quaternion.identity) as GameObject;
+            rocks[i] = Instantiate(rockPrefabs[rockIndex], 
+                Vector3.zero,
+                Quaternion.identity) as GameObject;
+            rocks[i].transform.SetParent(this.transform);
             rocks[i].SetActive(false);
         }
-
     }
-
     public static GameObject getTrees () {
         for (int i = 0; i < treeCount; i++)
         {
@@ -40,7 +42,6 @@ public class TerrainPool : MonoBehaviour
         }
         return null;
     }
-
     public static GameObject getRocks () {
         for (int i = 0; i < rockCount; i++)
         {
@@ -48,13 +49,6 @@ public class TerrainPool : MonoBehaviour
                 return rocks[i];
             }
         }
-
         return null;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
